1dee606d88c090487e8e26444d2193de2a32fa4f
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-03-09 12:45:22 +0300

    added test changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostMeta } from './regions/post-meta.entity';
-import { Posts } from './regions/posts.entity';
-import { RegionsModule } from './regions/regions.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // Делаем ConfigModule глобальным
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: '/run/mysqld/mysqld.sock', // Путь к сокету
+      username: 'user_c2c-samopis', // Имя пользователя
+      password: 'password', // Пароль
+      database: 'user_c2cv5', // Название базы данных
+      entities: [
+        // Ваши сущности здесь
+      ],
+      synchronize: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('DB_TYPE') as any,
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [Posts, PostMeta],
-        synchronize: true, // Внимание: используйте только в разработке!
-        // logging: true,
-      }),
-    }),
-    RegionsModule,
   ],
 })
 export class AppModule {}

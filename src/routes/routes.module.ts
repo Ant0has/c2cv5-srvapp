5090118f47bf4d3179d78045b47be203c542da09
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-03-12 11:45:04 +0300

    added fixes to routes module

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Posts } from 'src/regions/posts.entity';
+import { PostMeta } from 'src/regions/post-meta.entity';
+import { Regions } from 'src/regions/regions.entity';
+import { Routes } from 'src/regions/routes.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Posts, PostMeta, Regions, Routes])],
   providers: [RoutesService],
-  controllers: [RoutesController]
+  controllers: [RoutesController],
 })
 export class RoutesModule {}

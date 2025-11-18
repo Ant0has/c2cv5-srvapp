6928a9e43abaa188d7de6578e969e2eb30e60454
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-11-18 18:24:28 +0300

    fix: update field values in data-source.ts

--- data-source.ts
+++ data-source.ts
@@ -24,19 +24,12 @@ const { DataSource } = require('typeorm');
 
 module.exports = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'resam2171',
-  database: process.env.DB_DATABASE || 'city',
-  entities: [
-    'src/regions/post-meta.entity.ts',
-    'src/regions/posts.entity.ts',
-    'src/regions/regions.entity.ts',
-    'src/regions/routes.entity.ts',
-    'src/attractions/attraction-image.entity.ts',
-    'src/attractions/attraction.entity.ts'
-  ],
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'resam2171',
+  database: 'user_c2cv5',
+  entities: ['src/**/*.entity.ts'],
   migrations: ['migrations/*.ts'],
   synchronize: false,
 });
